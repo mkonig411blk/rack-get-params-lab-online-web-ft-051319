19add9d611e3ca30ef3a8d03beb5ecd3cfc964a0
Margot Konig <margot.konig@blackrock.com>
2019-06-23 17:10:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       end
       
     elsif req.path.match(/add/)
-      search_term = req.params["q"]
+      search_term = req.params["item"]
       resp.write handle_search_cart(search_term)
     
     else
